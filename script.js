03d114483e3dcfda21ba1396c637aecd5d77680c
meerasai <meerasaimarella9@gmail.com>
2024-02-28 10:49:31 +0530

    memorycard game

---
+++ script.js
@@ -0,0 +1,22 @@
+// const moves=document.getElementById("movescount");
+// const timevalue=document.getElementById("time")
+// const startbutton=document.getElementById("start")
+// const stopbutton=document.getElementById("stop")
+// const gamecontainer=document.querySelector("game-container");
+// const result=document.getElementById("result")
+// const controls=document.querySelector("controls-container");
+// let cards;
+// let interval;
+// let firstcard=false;
+// let secondcard=false;
+
+
+// const gridcontainer=document.querySelector(".grid-container");
+// let cards=[];
+// let firstcard,secondcard;
+// let score=0;
+// document.querySelector(".score").textcontent=score;
+// fetch("./data/cards.json")
+
+
+
